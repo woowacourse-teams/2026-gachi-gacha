5224a4947aff0257ce716cc294cf142cb4157bc4
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-08-05 08:50:14 +0900

    chore: 서버 세팅

---
+++ server/src/test/java/com/gachi/gacha/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gachi.gacha.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
