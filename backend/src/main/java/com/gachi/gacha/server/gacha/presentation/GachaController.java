b9a15107dd15b1a8d74e772f7426800a47f972b1
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-08-10 20:44:44 +0900

    refactor(GachaController): default 제거 및 uri 빌더를 사용한 location 지정

--- backend/src/main/java/com/gachi/gacha/server/gacha/presentation/GachaController.java
+++ backend/src/main/java/com/gachi/gacha/server/gacha/presentation/GachaController.java
@@ -4,29 +4,33 @@ import com.gachi.gacha.server.gacha.application.GachaService;
 import com.gachi.gacha.server.gacha.application.dto.GachaInfo;
 import com.gachi.gacha.server.gacha.presentation.dto.GachaCreateRequest;
 import com.gachi.gacha.server.gacha.presentation.dto.GachaResponse;
+import jakarta.validation.Valid;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/gachas")
 @RequiredArgsConstructor
 public class GachaController {
 
-    @Value("${url.gacha}")
-    private String DEFAULT_URL;
-
     private final GachaService gachaService;
 
     @PostMapping
-    public ResponseEntity<GachaResponse> createGacha(@RequestBody GachaCreateRequest request) {
-        GachaInfo gachaInfo = gachaService.addGacha(request);
-        GachaResponse gachaResponse = GachaResponse.fromInfo(gachaInfo);
-        return ResponseEntity.created(URI.create(DEFAULT_URL + gachaResponse.gachaId())).body(gachaResponse);
+    public ResponseEntity<GachaResponse> createGacha(@Valid @RequestBody GachaCreateRequest request) {
+        GachaInfo gachaInfo = gachaService.addGacha(request.toCommand());
+        GachaResponse gachaResponse = GachaResponse.from(gachaInfo);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(gachaResponse.gachaId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(gachaResponse);
     }
 }
