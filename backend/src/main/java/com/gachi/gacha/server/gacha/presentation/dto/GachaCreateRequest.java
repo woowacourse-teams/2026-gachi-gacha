1de471268ae3319fbb7f9f054ad844c82d226165
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-08-10 18:51:30 +0900

    refactor(GachaCreateRequest): 불필요한 필드 제거

--- backend/src/main/java/com/gachi/gacha/server/gacha/presentation/dto/GachaCreateRequest.java
+++ backend/src/main/java/com/gachi/gacha/server/gacha/presentation/dto/GachaCreateRequest.java
@@ -5,14 +5,12 @@ import com.gachi.gacha.server.gacha.domain.Gacha;
 public record GachaCreateRequest(
     String name,
     String caption,
-    String description,
     String thumbnailUrl
 ) {
     public Gacha toEntity() {
         return Gacha.builder()
                 .name(this.name())
                 .caption(this.caption())
-                .description(this.description())
                 .thumbnailUrl(this.thumbnailUrl())
                 .build();
     }
