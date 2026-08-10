9911399d5eb19fd880b0070f94bb6a0c98023ef8
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-08-10 16:43:00 +0900

    feat: 가챠, 매장, 매장 이미지, 매장 정보 클래스 정의

---
+++ backend/src/main/java/com/gachi/gacha/server/gacha/domain/Gacha.java
@@ -0,0 +1,28 @@
+package com.gachi.gacha.server.gacha.domain;
+
+import com.gachi.gacha.server.common.domain.BaseTimeEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Gacha extends BaseTimeEntity {
+
+    @Id
+    @Getter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String caption;
+    private String description;
+    private String thumbnailUrl;
+}
---
+++ backend/src/main/java/com/gachi/gacha/server/store/domain/Store.java
@@ -0,0 +1,21 @@
+package com.gachi.gacha.server.store.domain;
+
+import com.gachi.gacha.server.common.domain.BaseTimeEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@RequiredArgsConstructor
+public class Store extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String thumbnailUrl;
+    private Long latitude;
+    private Long longitude;
+}
