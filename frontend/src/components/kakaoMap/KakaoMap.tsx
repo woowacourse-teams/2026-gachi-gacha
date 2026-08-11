32578ae02760922acef79a4f0cfa572767d69d2f
TH_97 <hood3392@gmail.com>
2026-08-11 17:56:36 +0900

    refactor: 지도 컨테이너를 SDK 영역과 오버레이 영역으로 분리
    SDK가 소유하는 MapCanvas와 리액트가 소유하는 MapArea로 나눠, 지도 위에
    올릴 UI는 children으로 받도록 했다.

--- frontend/src/components/kakaoMap/KakaoMap.tsx
+++ frontend/src/components/kakaoMap/KakaoMap.tsx
@@ -1,3 +1,4 @@
+import type { ReactNode } from 'react';
 import styled from '@emotion/styled';
 
 import { useKakaoMap, type LatLngLiteral } from './useKakaoMap';
@@ -5,15 +6,31 @@ import { useKakaoMap, type LatLngLiteral } from './useKakaoMap';
 interface KakaoMapProps {
   center: LatLngLiteral;
   level?: number;
+  children?: ReactNode;
 }
 
-export default function KakaoMap({ center, level = 4 }: KakaoMapProps) {
+export default function KakaoMap({
+  center,
+  level = 4,
+  children,
+}: KakaoMapProps) {
   const { containerRef } = useKakaoMap({ center, level });
 
-  return <MapContainer ref={containerRef} />;
+  return (
+    <MapArea>
+      <MapCanvas ref={containerRef} />
+      {children}
+    </MapArea>
+  );
 }
 
-const MapContainer = styled.div`
+const MapArea = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
+
+const MapCanvas = styled.div`
   width: 100%;
   height: 100%;
 `;
