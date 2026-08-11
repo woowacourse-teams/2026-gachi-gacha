d62836b906a54c787b93731e60c8b494b48d37e0
TH_97 <hood3392@gmail.com>
2026-08-11 18:21:26 +0900

    chore: 카카오 지도 타입 패키지 도입
    직접 작성한 선언에는 지도 생성에 필요한 최소한만 있어서, CustomOverlay나
    setCenter 같은 API를 쓰려면 매번 손으로 추가해야 했다. 앞으로 사용할 범위를
    감당하기 어려워 공식 문서를 따라 작성된 패키지로 교체했다.

--- frontend/src/types/kakao.d.ts
+++ frontend/src/types/kakao.d.ts
@@ -1,18 +1,7 @@
+/// <reference types="kakao.maps.d.ts" />
+
 declare namespace kakao.maps {
   function load(callback: () => void): void;
-
-  class LatLng {
-    constructor(latitude: number, longitude: number);
-  }
-
-  interface MapOptions {
-    center: LatLng;
-    level?: number;
-  }
-
-  class Map {
-    constructor(container: HTMLElement, options: MapOptions);
-  }
 }
 
 interface Window {
