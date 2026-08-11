76eb49cb38d021b0aa8091b502f6a0697ebe91be
dahxxn <dahyeonhan4529@gmail.com>
2026-08-11 22:26:21 +0900

    refactor(common): 수동 수정 시간 갱신 메서드 제거

--- backend/src/main/java/com/gachi/gacha/server/common/domain/BaseTimeEntity.java
+++ backend/src/main/java/com/gachi/gacha/server/common/domain/BaseTimeEntity.java
@@ -18,8 +18,4 @@ public abstract class BaseTimeEntity {
 
     @LastModifiedDate
     private LocalDateTime updatedAt;
-
-    protected void markUpdated() {
-        this.updatedAt = LocalDateTime.now();
-    }
 }
