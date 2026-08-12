a893f2b26bc8d9a7121bfb2c10190bc2105f904b
Jihyun3478 <leejihyun3478@gmail.com>
2026-08-12 16:47:51 +0900

    feat: test 전용 application.yaml 파일 형식 수정

--- backend/src/test/java/com/gachi/gacha/server/ServerApplicationTests.java
+++ backend/src/test/java/com/gachi/gacha/server/ServerApplicationTests.java
@@ -2,9 +2,7 @@ package com.gachi.gacha.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
 @SpringBootTest
 class ServerApplicationTests {
 
