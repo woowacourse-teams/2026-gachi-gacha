a4c9040e97acd9a1ba93c6615e84096a2f8e84b7
Jihyun3478 <leejihyun3478@gmail.com>
2026-08-12 10:44:44 +0900

    fix: StoreImage에서 newImageUrl이 null일 때, NPE 대신 InvalidValueException을 발생시키도록 수정

--- backend/src/main/java/com/gachi/gacha/server/store/domain/StoreImage.java
+++ backend/src/main/java/com/gachi/gacha/server/store/domain/StoreImage.java
@@ -1,5 +1,7 @@
 package com.gachi.gacha.server.store.domain;
 
+import com.gachi.gacha.server.common.exception.ErrorCode;
+import com.gachi.gacha.server.common.exception.InvalidValueException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -38,7 +40,9 @@ public class StoreImage {
     }
 
     public void changeImageUrl(final String newImageUrl) {
-        Objects.requireNonNull(newImageUrl, "이미지 URL은 필수값입니다.");
+        if (newImageUrl == null) {
+            throw new InvalidValueException(ErrorCode.INVALID_STORE_IMAGE_POLICY);
+        }
         this.imageUrl = newImageUrl;
     }
 }
