00cb94535a89158d2977d9abce2561f303e594fc
Jihyun3478 <leejihyun3478@gmail.com>
2026-09-17 15:33:17 +0900

    feat: S3 업로드 타임아웃 및 파일 용량 제한 설정

--- backend/src/main/java/com/gachi/gacha/server/common/infra/config/ImageConfig.java
+++ backend/src/main/java/com/gachi/gacha/server/common/infra/config/ImageConfig.java
@@ -1,8 +1,11 @@
 package com.gachi.gacha.server.common.infra.config;
 
+import java.time.Duration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.http.apache.ApacheHttpClient;
+import software.amazon.awssdk.http.apache.ApacheHttpClient.Builder;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -14,8 +17,12 @@ public class ImageConfig {
 
     @Bean
     public S3Client s3Client() {
+        Builder builder = ApacheHttpClient.builder()
+                .socketTimeout(Duration.ofSeconds(100));
+
         return S3Client.builder()
                 .region(Region.of(region))
+                .httpClientBuilder(builder)
                 .build();
     }
 }
