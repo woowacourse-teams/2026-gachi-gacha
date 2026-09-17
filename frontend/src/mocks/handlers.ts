d98231df4b1ff40fa6e6ced81a5651a903dda37c
binggwa <qudrhks317@gmail.com>
2026-09-17 16:05:02 +0900

    test(gacha-search): 가챠 검색 MSW 핸들러 추가
    가챠 목록 조회 요청을 목 페이지 응답과 연결한다.
    
    페이지 쿼리를 검증하고 잘못된 요청에는 400 오류를 반환한다.

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,3 +1,4 @@
+import { gachaSearchHandlers } from '@/features/gachaSearch/mocks/gachaSearchHandlers';
 import { searchHandlers } from '@/routes/search/mocks/searchHandlers';
 
-export const handlers = [...searchHandlers];
+export const handlers = [...gachaSearchHandlers, ...searchHandlers];
