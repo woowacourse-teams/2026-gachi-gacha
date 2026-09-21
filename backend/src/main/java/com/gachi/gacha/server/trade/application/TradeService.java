acfd571d567fcf76b86a1f9789b8ae6ad8e6a21c
Jihyun3478 <leejihyun3478@gmail.com>
2026-09-21 11:37:48 +0900

    refactor: 사용하지 않는 import 제거

--- backend/src/main/java/com/gachi/gacha/server/trade/application/TradeService.java
+++ backend/src/main/java/com/gachi/gacha/server/trade/application/TradeService.java
@@ -29,7 +29,6 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
