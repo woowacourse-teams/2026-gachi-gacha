2ffba4c8ae11825a0ce97b76f1c19b372a9b3ecd
binggwa <qudrhks317@gmail.com>
2026-09-21 16:36:23 +0900

    feat(app): 검색 결과 경로 연결

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,9 @@
+import { SearchRoute } from '@/routes/search/route';
+
 export default function App() {
+  if (window.location.pathname === '/search') {
+    return <SearchRoute />;
+  }
+
   return null;
 }
