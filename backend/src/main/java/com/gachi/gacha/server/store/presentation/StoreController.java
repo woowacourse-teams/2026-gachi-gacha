9d88cd6f36f2888cab1de9a3f7347a5671a7f048
Bello🥔 <park.juntae6942@gmail.com>
2026-09-23 16:22:49 +0900

    [REFACTOR] 잘못된 응답 포맷 변경 (#114)
    * refactor: 잘못된 응답 수정
    
    * refactor: 불필요한 import 제거

--- backend/src/main/java/com/gachi/gacha/server/store/presentation/StoreController.java
+++ backend/src/main/java/com/gachi/gacha/server/store/presentation/StoreController.java
@@ -16,7 +16,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,18 +31,18 @@ public class StoreController {
     private final StoreGachaService storeGachaService;
 
     @GetMapping("/nearby")
-    public ResponseEntity<BaseResponse<StoreNearbyResponse>> readNearbyStores(
+    public BaseResponse<StoreNearbyResponse> readNearbyStores(
             @RequestParam(required = false) final Double latitude,
             @RequestParam(required = false) final Double longitude,
             @RequestParam(defaultValue = "3000") final Integer radius,
             @RequestParam(required = false) final Integer floor
     ) {
         StoreNearbyResult result = storeService.findNearbyStores(latitude, longitude, radius, floor);
-        return ResponseEntity.ok(BaseResponse.ok(StoreNearbyResponse.from(result)));
+        return BaseResponse.ok(StoreNearbyResponse.from(result));
     }
 
     @GetMapping("/nearby/{gachaId}")
-    public ResponseEntity<BaseResponse<StoreNearbyResponse>> readNearbyStores(
+    public BaseResponse<StoreNearbyResponse> readNearbyStores(
             @RequestParam(required = false) final Double latitude,
             @RequestParam(required = false) final Double longitude,
             @RequestParam(defaultValue = "3000") final Integer radius,
@@ -51,7 +50,7 @@ public class StoreController {
             @PathVariable final Long gachaId
     ) {
         StoreNearbyResult result = storeService.findNearbyStoresByGachaId(latitude, longitude, radius, floor, gachaId);
-        return ResponseEntity.ok(BaseResponse.ok(StoreNearbyResponse.from(result)));
+        return BaseResponse.ok(StoreNearbyResponse.from(result));
     }
 
     @GetMapping
@@ -63,11 +62,11 @@ public class StoreController {
     }
 
     @GetMapping("/{storeId}")
-    public ResponseEntity<BaseResponse<StoreDetailResponse>> readStore(
+    public BaseResponse<StoreDetailResponse> readStore(
             @PathVariable final Long storeId
     ) {
         StoreDetailResult result = storeService.getStore(storeId);
-        return ResponseEntity.ok(BaseResponse.ok(StoreDetailResponse.from(result)));
+        return BaseResponse.ok(StoreDetailResponse.from(result));
     }
 
     @GetMapping("/{storeId}/gachas")
